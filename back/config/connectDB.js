f9d64b06ecfe2e041ae855fe23e00340274975de
Meher-ak <akermi.meher@gmail.com>
2023-11-18 17:45:44 +0100

    update mongoDB connection

--- back/config/connectDB.js
+++ back/config/connectDB.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI);
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.yqucx.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`
+    );
     console.log("you are connected to DB");
   } catch (error) {
     console.log("database is not connected", error);
